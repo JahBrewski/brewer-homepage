91253146917fd176890d6879ebe25f7ebfdd1c54
Joel Brewer <joelbrewer01@gmail.com>
2013-09-02 21:21:36 -0500

    added cityscape background image plus lots of styling shiz

--- js/home.js
+++ js/home.js
@@ -11,4 +11,27 @@ $(document).ready(function(){
                 window.location.hash = target;
             });
         });
+
+        function moveScroller() {
+            var move = function() {
+                var st = $(window).scrollTop();
+                var ot = $("#scroller-anchor").offset().top;
+                var s = $("#scroller");
+                if(st > ot) {
+                    s.css({
+                        position: "fixed",
+                        top: "0px"
+                    });
+                } else {
+                    if(st <= ot) {
+                        s.css({
+                            position: "relative",
+                            top: ""
+                        });
+                    }
+                }
+            };
+            $(window).scroll(move);
+            move();
+        }
 });
