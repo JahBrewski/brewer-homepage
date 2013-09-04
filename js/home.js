d9c591fec2f14b9a580ce4102c440b4543507685
Joel Brewer <joelbrewer01@gmail.com>
2013-09-04 18:13:59 -0500

    skillset status bar animation working

--- js/home.js
+++ js/home.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
 
+    //Scroll to tag
         $('a[href^="#"]').on('click',function (e) {
             e.preventDefault();
-
             var target = this.hash,
             $target = $(target);
-
             $('html, body').stop().animate({
                 'scrollTop': $target.offset().top
             }, 900, 'swing', function () {
@@ -18,7 +17,6 @@ $(document).ready(function(){
                 offset     = $navigation.offset(),
                 topPadding = 15;
 
-
         $window.scroll(function() {
                 if ($window.scrollTop() > offset.top) {
                 $navigation.addClass('fixed');
@@ -27,19 +25,34 @@ $(document).ready(function(){
         }
     });
 
-        $(function() {
-    $('.banner').unslider({
-            speed: 500,               //  The speed to animate each slide (in milliseconds)
-            delay: 3000,              //  The delay between slide animations (in milliseconds)
-            complete: function() {},  //  A function that gets called after every slide animation
-            keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-            dots: true,               //  Display dot navigation
-            fluid: false              //  Support responsive design. May break non-responsive designs
-    });
+        function elementInViewport(el) {
+          var top = el.offsetTop;
+          var left = el.offsetLeft;
+          var width = el.offsetWidth;
+          var height = el.offsetHeight;
 
-            });
+          while(el.offsetParent) {
+            el = el.offsetParent;
+            top += el.offsetTop;
+            left += el.offsetLeft;
+          }
 
-        
- 
+          return (
+            top >= window.pageYOffset &&
+            left >= window.pageXOffset &&
+            (top + height) <= (window.pageYOffset + window.innerHeight) &&
+            (left + width) <= (window.pageXOffset + window.innerWidth)
+          );
+        }
+            function checkAnimation() {
+        $('.html-css').each(function() {
+            if (!$(this).hasClass('start') && elementInViewport(this)) {
+                $(this).addClass('start');
+            }
+        });
+    }
+        $(window).scroll(function() {
+             checkAnimation();
+         });
 
 });
