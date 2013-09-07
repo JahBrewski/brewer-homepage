3f13cae857c59c99c62b73b7b166fee18f07dbbe
Joel Brewer <joelbrewer01@gmail.com>
2013-09-07 17:02:08 -0500

    Updated about copy, styling changes, added min-width

--- js/home.js
+++ js/home.js
@@ -55,4 +55,27 @@ $(document).ready(function(){
              checkAnimation();
          });
 
+        var modal = (function(){
+            var 
+            method = {},
+            $overlay,
+            $modal,
+            $content,
+            $close;
+
+            // Append the HTML
+
+            // Center the modal in the viewport
+            method.center = function () {};
+
+            // Open the modal
+            method.open = function (settings) {};
+
+            // Close the modal
+            method.close = function () {};
+
+            return method;
+
+        }()); 
+
 });
